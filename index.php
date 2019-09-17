2fd8eb9aec0d602595edff05a98cba4136f02061
Arthur <arthur@LAPTOP-5N0RBHOG.localdomain>
2019-09-17 13:53:01 +0200

    add data interval

--- index.php
+++ index.php
@@ -23,32 +23,33 @@
 
  <!-- Carousel -->
  
-  <section>
+  <section id="carousel">
 
-  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class ="container">
+  <div class="row">
+  <div class="col-10 offset-1 ">
+
+  <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel" data-interval="2000">
   <div class="carousel-inner">
+
     <div class="carousel-item active">
-      <img class="d-block w-100" src="https://via.placeholder.com/1000x400" alt="First slide">
+      <img class="d-block w-100" src="images/Idrink.jpg" alt="First slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="https://via.placeholder.com/1000x400" alt="Second slide">
+      <img class="d-block w-100" src="images/Ipee.jpg" alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="https://via.placeholder.com/1000x400" alt="Third slide">
+      <img class="d-block w-100" src="images/Itsmakeatree.jpg" alt="Third slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="https://via.placeholder.com/1000x400" alt="Fourth slide">
+      <img class="d-block w-100" src="images/Itsgreen.jpg" alt="Fourth slide">
     </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
+
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
 
 
 
