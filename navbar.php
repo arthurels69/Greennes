63d3ac5e0850dd813aa85c0b06831ba80a36312a
Thomas <tponthoreau@gmail.com>
2019-09-18 09:09:15 +0200

    debug navbar

--- navbar.php
+++ navbar.php
@@ -1,44 +1,68 @@
-<nav class="title navbar navbar-expand-sm navbar-light bg-white">
-  <div class="container-fluid">
-    <div class="row mr-auto ml-auto">
-      <div class="col-12">
-        <a href="#" class="navbar-brand h1 mb-0 greenness">Greenness</a>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="style.css"><link href="https://fonts.googleapis.com/css?family=Kaushan+Script&display=swap" rel="stylesheet">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Greenness</title>
 
-      </div>
-    </div>
-  </div>
-
-      <!-- Navbar -->
-  <div class="navbar navbar-expand-sm navbar-light bg-white">
-
-    <div class="container-fluid">
-      <div class="row ml-auto mr-auto">
-        <div class="col-12">
-
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+          <!-- Titre -->
           
-          <a href="#" class="navbar-brand"></a>
-          <button class="btn btn-success navbar-toggler" type="button" data-toggle="dropdown" data-target="#navbarMenu">
-            <span class="navbar-toggler-icon "></span>
-          </button>
+  <nav>
+    <h1><a href="#" class="navbar-brand h1 mb-0 greenness">Greenness</a></h1>
 
-
-            <div class="collapse navbar-collapse" id="navbarMenu">
-              <ul class="navbar-nav">
-
-                <li class="nav-item col">
-                  <a href="#home" class="nav-link">Home</a>
-                </li>
-
-                <li class="nav-item col">
-                  <a href="#produits" class="nav-link">Produits</a>
-                </li>
-                <li class="nav-item col">
-                  <a href="#contact" class="nav-link">Contact</a>
-                </li>
-              </ul>
-            </div>
         </div>
       </div>
     </div>
-  </div>
-</nav>
+
+       <!-- Navbar -->
+    <div class="navbar navbar-expand-sm navbar-light bg-white">
+
+      <div class="container-fluid">
+        <div class="row ml-auto mr-auto">
+          <div class="col-12">
+
+           
+            <a href="#" class="navbar-brand"></a>
+            <button class="btn btn-success navbar-toggler" type="button" data-toggle="dropdown" data-target="#navbarMenu">
+              <span class="navbar-toggler-icon "></span>
+            </button>
+
+
+              <div class="collapse navbar-collapse" id="navbarMenu">
+                <ul class="navbar-nav">
+
+                  <li class="nav-item col">
+                    <a href="#home" class="nav-link">Home</a>
+                  </li>
+
+                  <li class="nav-item col">
+                    <a href="#produits" class="nav-link">Produits</a>
+                  </li>
+                  <li class="nav-item col">
+                    <a href="#contact" class="nav-link">Contact</a>
+                  </li>
+                </ul>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <!-- END / Navbar -->
+
+
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+<img src="">
