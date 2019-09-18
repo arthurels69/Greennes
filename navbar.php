8771001a49ceb098b38ab41991c138e9e80b9992
Martin Rivière <martin.riviere@gmail.com>
2019-09-18 16:25:41 +0200

    modif centering of navbar elements

--- navbar.php
+++ navbar.php
@@ -1,21 +1,20 @@
-
       <!-- Navbar -->
 <nav class="navbar navbar-expand-sm navbar-light bg-light sticky-top">
-  <img class="glogo" src="images/glogo.png" alt="logo greenness"></img>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  <img class="glogo" src="images/glogo.png" alt="logo greenness">
+  <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ml-auto mr-auto">
-        <li>
-          <a class="nav-link" href="index.php">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="product.php">Produits</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="contact.php">Contact</a>
-        </li>
-      </ul>
-   </div>
+  <div class="collapse navbar-collapse text-center" id="navbarNav">
+    <ul class="navbar-nav ml-auto mr-auto">
+      <li>
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="product.php">Produits</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contact.php">Contact</a>
+      </li>
+    </ul>
+  </div>
 </nav>
