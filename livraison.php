f00d777a3ee5302778286e98b76ead1dc1f3fe3b
Arthur <arthur@LAPTOP-5N0RBHOG.localdomain>
2019-09-18 15:24:52 +0200

    ajout barre livraison

--- livraison.php
+++ livraison.php
@@ -9,13 +9,13 @@
         <p><b> Livraison  domicile</b> <br><br>Gratuite dés 10l achetés</p>
 
     </div>
-    <div class="col-12 col-md-4 ">
+    <div id="rocket" class="col-12 col-md-4 ">
     <p class="fa fa-rocket fa-4x"><p>
         <p><b>Livraison express</b><br> <br>Garantie en 24H </p>
     </div>
     <div class="col-12 col-md-4 ">
     <p class="fa fa-gift fa-4x"><p>
-        <p> <b>Avantages</b><br> <br>Bénéficiez de 5% tous les 100e</p>
+        <p> <b>Avantages</b><br> <br>Bénéficiez <br> de 5% tous les 100e d'achats</p>
         <p>
     </div>
   </div>
