6734285e7b7623a082c45fee4b9f6c4795020767
Martin Rivière <martin.riviere@gmail.com>
2019-09-18 15:04:11 +0200

    add age check feature

---
+++ age_check.js
@@ -0,0 +1,30 @@
+if (!(sessionStorage.getItem('age'))) {
+    ageCheck = document.getElementById("ageCheck")
+    ageCheck.showModal()
+    form = document.querySelector("form")
+    birthDate = document.getElementById("birthDate")
+    confirmBtn = document.getElementById("confirmBtn")
+    cancelButton = document.getElementById("cancel")
+    const currentDate = new Date()
+    currentYear = currentDate.getFullYear()
+    form.addEventListener('submit', (e) => {
+        if (birthDate.value === "") {
+            e.preventDefault()
+        }
+        else if (currentYear - birthDate.value.substring(0, 4) < 18) {
+            e.preventDefault()
+            pElt = document.createElement('p')
+            pElt.classList.add("text-center")
+            if (birthDate.value.substring(0, 4) > currentYear) {
+                pElt.textContent = "Vous n'êtes pas né et n'avez pas accès à ce site"
+            } else {
+                pElt.textContent = "Vous êtes mineur et n'avez pas accès à ce site"
+            }
+            ageCheck.innerHTML = ""
+            ageCheck.appendChild(pElt)
+            setTimeout(() => window.history.back(), 3000)
+        } else {
+            sessionStorage.setItem('age', 'majeur')
+        }
+    })
+}  
