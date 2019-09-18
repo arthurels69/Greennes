d3ed7bbea5dcdb9b478d8a30e2a2b4f509c1f129
Pierre-Alain Monney <pierrealain.monney@gmail.com>
2019-09-18 10:20:16 +0200

    ajout fichier pop up

--- card_product.php
+++ card_product.php
@@ -1,165 +1,259 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<link rel="stylesheet" href="style.css">
+		<title>Card_product</title>
+	</head>
+	<body>
+		<div>
+			<div class="container bloc_product">
+				<div class="row">
+					<!-- Ceci est une carte -->
+					<div class="card_product col-12 col-lg-6">
+						<a href="#">
+						
+					
+							<div class="card" style="height: 100%;">
+								<div class="row no-gutters">
+									<div class="col-12 col-sm-4">
+										<img src="./images/Canette bleue.jpg" data-toggle="modal" data-target="#infosbleue" class="img-fluid" alt="canette bleue">
+									</div>
+									<div class="modal" id="infosbleue">
+									<div class="modal-dialog">
+									<div class="modal-content">
+									
+									<?php include('tarifbleue.php'); ?>
+								</div>
+								</div>
+								</div>
+								
+						
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-
-    <title>Card_product</title>
-  </head>
-  <body>
-
-    <div>
-	    <div class="container bloc_product">
-	    	<div class="row">
-	    		<!-- Ceci est une carte -->
-	    		<div class="card_product col-12 col-lg-6">
-	    			<a href="#">
-				    <div class="card" style="height: 100%;">
-				    	<div class="row no-gutters">
-				    		<div class="col-12 col-sm-4">
-					  			<img src="./images/Canette bleue.jpg" class="img-fluid" alt="canette bleue">
-					  		</div>
-					  		<div class="col-12 col-sm-8" style="background-color: #F7F7F7;border-radius: 20px;">
-						  		<div class="card-body">
-						   			<h5 class="card-title">Bière bleue</h5>
-						    		<p class="card-text">Cette bière blanche trouble combine les saveurs fruitées des levures belges avec le côté épicé de la graine de coriandre et des écorces d’oranges. Très agréable à boire car elle est très pétillante. Servir frappée.</p>
-						    		<table class="table table-borderless">
-									  <tbody>
-									    <tr>
-									      <th scope="row">Style</th>
-									      <td>Blanche</td>
-									    </tr>
-									    <tr>
-									      <th scope="row">Contenu</th>
-									      <td>33cl</td>
-									    </tr>
-									    <tr>
-									      <th scope="row">Degré d'alcool</th>
-									      <td colspan="2">5,4%</td>
-									    </tr>
-									  </tbody>
-									</table>
-						  		</div>
-					  		</div>
-					  	</div>
+									<div class="col-12 col-sm-8" style="background-color: #F7F7F7;border-radius: 20px;">
+										<div class="card-body">
+											<h5 class="card-title">
+												Bière bleue
+											</h5>
+											<p class="card-text">
+												Cette bière blanche trouble combine les saveurs fruitées des levures belges avec le côté épicé de la graine de coriandre et des écorces d’oranges. Très agréable à boire car elle est très pétillante. Servir frappée.
+											</p>
+											<table class="table table-borderless">
+												<tbody>
+													<tr>
+														<th scope="row">
+															Style
+														</th>
+														<td>
+															Blanche
+														</td>
+													</tr>
+													<tr>
+														<th scope="row">
+															Contenu
+														</th>
+														<td>
+															33cl
+														</td>
+													</tr>
+													<tr>
+														<th scope="row">
+															Degré d'alcool
+														</th>
+														<td colspan="2">
+															5,4%
+														</td>
+													</tr>
+												</tbody>
+											</table>
+										</div>
+									</div>
+								</div>
+							</div>
+						</a>
 					</div>
-					</a>
-				</div>
-				<!-- Ceci est une carte -->
-	    		<div class="card_product col-12 col-lg-6">
-	    			<a href="#">
-				    <div class="card" style="height: 100%;">
-				    	<div class="row no-gutters" style="height: 100%;">
-				    		<div class="col-12 col-sm-4">
-					  			<img src="./images/Canette rouge.jpg" class="img-fluid" alt="canette rouge">
-					  		</div>
-					  		<div class="col-12 col-sm-8" style="background-color: #F7F7F7;border-radius: 20px;">
-						  		<div class="card-body" >
-						   			<h5 class="card-title">Bière rouge</h5>
-						    		<p class="card-text">Une bière d’hiver rousse tirant sur le brun. Très parfumée, avec une agréable saveur de clou de girofle et de cannelle. Le goût reste cependant plutôt frais et léger.</p>
-						    		<table class="table table-borderless">
-									  <tbody>
-									    <tr>
-									      <th scope="row">Style</th>
-									      <td>Ambré</td>
-									    </tr>
-									    <tr>
-									      <th scope="row">Contenu</th>
-									      <td>33cl</td>
-									    </tr>
-									    <tr>
-									      <th scope="row">Degré d'alcool</th>
-									      <td colspan="2">6,8%</td>
-									    </tr>
-									  </tbody>
-									</table>
-						  		</div>
-					  		</div>
-					  	</div>
+					<!-- Ceci est une carte -->
+					<div class="card_product col-12 col-lg-6">
+						<a href="#">
+							<div class="card" style="height: 100%;">
+								<div class="row no-gutters" style="height: 100%;">
+									<div class="col-12 col-sm-4">
+										<img src="./images/Canette rouge.jpg" data-toggle="modal" data-target="#infosrouge" class="img-fluid" alt="canette rouge">
+									</div>
+									<div class="modal" id="infosrouge">
+								<div class="modal-dialog">
+									<div class="modal-content">
+						            <?php include('tarifrouge.php'); ?>
+									</div>
+								</div>
+							</div>
+									<div class="col-12 col-sm-8" style="background-color: #F7F7F7;border-radius: 20px;">
+										<div class="card-body" >
+											<h5 class="card-title">
+												Bière rouge
+											</h5>
+											<p class="card-text">
+												Une bière d’hiver rousse tirant sur le brun. Très parfumée, avec une agréable saveur de clou de girofle et de cannelle. Le goût reste cependant plutôt frais et léger.
+											</p>
+											<table class="table table-borderless">
+												<tbody>
+													<tr>
+														<th scope="row">
+															Style
+														</th>
+														<td>
+															Ambré
+														</td>
+													</tr>
+													<tr>
+														<th scope="row">
+															Contenu
+														</th>
+														<td>
+															33cl
+														</td>
+													</tr>
+													<tr>
+														<th scope="row">
+															Degré d'alcool
+														</th>
+														<td colspan="2">
+															6,8%
+														</td>
+													</tr>
+												</tbody>
+											</table>
+										</div>
+									</div>
+								</div>
+							</div>
+						</a>
 					</div>
-					</a>
-				</div>
-				<!-- Ceci est une carte -->
-	    		<div class="card_product col-12 col-lg-6">
-	    			<a href="#">
-				    <div class="card" style="height: 100%;">
-				    	<div class="row no-gutters">
-				    		<div class="col-12 col-sm-4">
-					  			<img src="./images/Canette verte.jpg" class="img-fluid" alt="canette verte">
-					  		</div>
-					  		<div class="col-12 col-sm-8" style="background-color: #F7F7F7;border-radius: 20px;">
-						  		<div class="card-body">
-						   			<h5 class="card-title">Bière verte</h5>
-						    		<p class="card-text">Bière verte aux saveurs herbales et légèrement amères. Au nez, elle est mentholée. La finale en bouche est courte et assez herbale.Le gluten est éliminé au cours du processus de brassage – chaque lot est contrôlé à un niveau inférieur à 20 ppm.</p>
-						    		<table class="table table-borderless">
-									  <tbody>
-									    <tr>
-									      <th scope="row">Style</th>
-									      <td>Blonde</td>
-									    </tr>
-									    <tr>
-									      <th scope="row">Contenu</th>
-									      <td>33cl</td>
-									    </tr>
-									    <tr>
-									      <th scope="row">Degré d'alcool</th>
-									      <td colspan="2">5,9%</td>
-									    </tr>
-									  </tbody>
-									</table>
-						  		</div>
-					  		</div>
-					  	</div>
+					<!-- Ceci est une carte -->
+					<div class="card_product col-12 col-lg-6">
+						<a href="#">
+							<div class="card" style="height: 100%;">
+								<div class="row no-gutters">
+									<div class="col-12 col-sm-4">
+										<img src="./images/Canette verte.jpg" data-toggle="modal" data-target="#infosverte" class="img-fluid" alt="canette verte">
+									</div>
+									<div class="modal" id="infosverte">
+								<div class="modal-dialog">
+									<div class="modal-content">
+						            <?php include('tarifverte.php'); ?>
+									</div>
+								</div>
+							</div>
+									<div class="col-12 col-sm-8" style="background-color: #F7F7F7;border-radius: 20px;">
+										<div class="card-body">
+											<h5 class="card-title">
+												Bière verte
+											</h5>
+											<p class="card-text">
+												Bière verte aux saveurs herbales et légèrement amères. Au nez, elle est mentholée. La finale en bouche est courte et assez herbale.Le gluten est éliminé au cours du processus de brassage – chaque lot est contrôlé à un niveau inférieur à 20 ppm.
+											</p>
+											<table class="table table-borderless">
+												<tbody>
+													<tr>
+														<th scope="row">
+															Style
+														</th>
+														<td>
+															Blonde
+														</td>
+													</tr>
+													<tr>
+														<th scope="row">
+															Contenu
+														</th>
+														<td>
+															33cl
+														</td>
+													</tr>
+													<tr>
+														<th scope="row">
+															Degré d'alcool
+														</th>
+														<td colspan="2">
+															5,9%
+														</td>
+													</tr>
+												</tbody>
+											</table>
+										</div>
+									</div>
+								</div>
+							</div>
+						</a>
 					</div>
-					</a>
-				</div>
-				<!-- Ceci est une carte -->
-	    		<div class="card_product col-12 col-lg-6">
-	    			<a href="#">
-				    <div class="card" style="height: 100%;">
-				    	<div class="row no-gutters" style="height: 100%;">
-				    		<div class="col-12 col-sm-4">
-					  			<img src="./images/Canette jaune.jpg" class="img-fluid" alt="canette jaune">
-					  		</div>
-					  		<div class="col-12 col-sm-8" style="background-color: #F7F7F7;border-radius: 20px;">
-						  		<div class="card-body">
-						   			<h5 class="card-title">Bière jaune</h5>
-						    		<p class="card-text">Bière trappiste parfaitement équilibrée à la robe brun-rouge. Léger goût sucré et arômes de chocolat, de fruits secs, de pain grillé et de caramel.</p>
-						    		<table class="table table-borderless">
-									  <tbody>
-									    <tr>
-									      <th scope="row">Style</th>
-									      <td>Brune</td>
-									    </tr>
-									    <tr>
-									      <th scope="row">Contenu</th>
-									      <td>33cl</td>
-									    </tr>
-									    <tr>
-									      <th scope="row">Degré d'alcool</th>
-									      <td colspan="2">8,0%</td>
-									    </tr>
-									  </tbody>
-									</table>
-						  		</div>
-					  		</div>
-					  	</div>
+					<!-- Ceci est une carte -->
+					<div class="card_product col-12 col-lg-6">
+						<a href="#">
+							<div class="card" style="height: 100%;">
+								<div class="row no-gutters" style="height: 100%;">
+									<div class="col-12 col-sm-4">
+										<img src="./images/Canette jaune.jpg" data-toggle="modal" data-target="#infosjaune" class="img-fluid" alt="canette jaune">
+									</div>
+									<div class="modal" id="infosjaune">
+								<div class="modal-dialog">
+									<div class="modal-content">
+						            <?php include('tarifjaune.php'); ?>
+									</div>
+								</div>
+							</div>
+									<div class="col-12 col-sm-8" style="background-color: #F7F7F7;border-radius: 20px;">
+										<div class="card-body">
+											<h5 class="card-title">
+												Bière jaune
+											</h5>
+											<p class="card-text">
+												Bière trappiste parfaitement équilibrée à la robe brun-rouge. Léger goût sucré et arômes de chocolat, de fruits secs, de pain grillé et de caramel.
+											</p>
+											<table class="table table-borderless">
+												<tbody>
+													<tr>
+														<th scope="row">
+															Style
+														</th>
+														<td>
+															Brune
+														</td>
+													</tr>
+													<tr>
+														<th scope="row">
+															Contenu
+														</th>
+														<td>
+															33cl
+														</td>
+													</tr>
+													<tr>
+														<th scope="row">
+															Degré d'alcool
+														</th>
+														<td colspan="2">
+															8,0%
+														</td>
+													</tr>
+												</tbody>
+											</table>
+										</div>
+									</div>
+								</div>
+							</div>
+						</a>
 					</div>
-					</a>
 				</div>
 			</div>
 		</div>
-	</div>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	</body>
 </html>
