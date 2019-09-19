b87ac7b843b9d2e067fbf87682578f4171ee8a20
Arthur <arthur@LAPTOP-5N0RBHOG.localdomain>
2019-09-19 14:23:58 +0200

    ajout texte francais

--- index.php
+++ index.php
@@ -41,28 +41,27 @@
 
   <div class="container1">
           <div class="box">
-            <span class="bullet-title">Protect The Nature</span>
+            <span class="bullet-title">Protège la nature</span>
             <img class="bullet-image"src="/images/1.png" alt="Protect the nature">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. Aliquam erat volutpat. Donec placerat nisl magna, et faucibus arcu condimentum sed.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. Aliquam erat volutpat. Donec placerat nisl magna, et faucibus arcu condimentum sed.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. Aliquam erat volutpat. Donec placerat nisl magna, et faucibus arcu condimentum sed.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </div>
-  
+            <p>Grâce aux nouvelles techniques développées par nos ingénieurs nous réussissons à brasser notre bière en respectant l’environnement. Nous garantissons le zéro émission carbone pour chaque procédé ( énergie solaire, transport électrique, agriculture biologique .. ).De plus nous vous recommandons de faire vos besoins en pleine nature après consommation, en effet la Greennes permet de générer de l’engrais bio qui accélère grandement la croissance des végétaux.<p>
+  </div>
           <div class="box">
-            <span class="bullet-title">Protect Your Health</span>
+            <span class="bullet-title">Protège ta santé</span>
             <img class="bullet-image" src="/images/2.png" alt="Protect your health">      
-            <p class="cup">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. Aliquam erat volutpat. Donec placerat nisl magna, et faucibus arcu condimentum sed.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. Aliquam erat volutpat. Donec placerat nisl magna, et faucibus arcu condimentum sed.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. Aliquam erat volutpat. Donec placerat nisl magna, et faucibus arcu condimentum sed.Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <p class="cup">Notre bière est la toute première boisson alcoolisée totalement inoffensive pour la santé. Grâce à tous les oligo-éléments générés lors du brassage, la Greennes prévient des risques cardio-vasculaires. Au revoir la gueule de bois et bonjour les lendemains actifs ! Aucun effet secondaire lors de surconsommation de notre produit, au contraire, un état de forme optimum le lendemain se fait ressentir, non vous ne rêvez pas !! </p>
           </div>
   
           <div class="box">
-            <span class="bullet-title">Protect Your Wallet</span>
+            <span class="bullet-title">Protège ton portefeuille</span>
             <img class="bullet-image" src="/images/3.png" alt="Protect your wallet">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. Aliquam erat volutpat. Donec placerat nisl magna, et faucibus arcu condimentum sed.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. Aliquam erat volutpat. Donec placerat nisl magna, et faucibus arcu condimentum sed.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. Aliquam erat volutpat. Donec placerat nisl magna, et faucibus arcu condimentum sed.Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+            <p>Le rapport qualité prix de la Greennes défi toute concurrence, nous avons développé un procédé original qui permet de contenir l’équivalent de trois bières dans une. Il suffit tout simplement de mélanger chaque tiers de celle-ci avec de l’eau, goût, texture et onctuosité sont préservés. Satisfait ou remboursé, à vous de voir ! </p>
           </div>
   </div>
 
 <!--Youtube video section-->
  </div>  
 
-<h2>We are not only making beers, we are purchasing a dream</h2>
+<h2>Nous ne faisons pas que de la bière, nous poursuivons un rêve</h2>
 <div class="video">
         
         <div class="video-part2">
