3cb3991455f90e041db4708ba39352a2050cb790
Martin Rivière <martin.riviere@gmail.com>
2019-09-19 16:45:44 +0200

    some text changes

--- index.php
+++ index.php
@@ -43,18 +43,18 @@
           <div class="box">
             <span class="bullet-title">Protège la nature</span>
             <img class="bullet-image"src="/images/1.png" alt="Protect the nature">
-            <p>Grâce aux nouvelles techniques développées par nos ingénieurs nous réussissons à brasser notre bière en respectant l’environnement. Nous garantissons le zéro émission carbone pour chaque procédé ( énergie solaire, transport électrique, agriculture biologique .. ).De plus nous vous recommandons de faire vos besoins en pleine nature après consommation, en effet la Greennes permet de générer de l’engrais bio qui accélère grandement la croissance des végétaux.<p>
-  </div>
+            <p>Grâce aux nouvelles techniques développées par nos ingénieurs nous réussissons à brasser notre bière en respectant l’environnement. Nous garantissons le zéro émission carbone pour chaque procédé (énergie solaire, transport électrique, agriculture biologique...). De plus nous vous recommandons de faire vos besoins en pleine nature après consommation, en effet la Greenness permet de générer de l’engrais bio qui accélère grandement la croissance des végétaux.<p>
+            </div>
           <div class="box">
             <span class="bullet-title">Protège ta santé</span>
             <img class="bullet-image" src="/images/2.png" alt="Protect your health">      
-            <p class="cup">Notre bière est la toute première boisson alcoolisée totalement inoffensive pour la santé. Grâce à tous les oligo-éléments générés lors du brassage, la Greennes prévient des risques cardio-vasculaires. Au revoir la gueule de bois et bonjour les lendemains actifs ! Aucun effet secondaire lors de surconsommation de notre produit, au contraire, un état de forme optimum le lendemain se fait ressentir, non vous ne rêvez pas !! </p>
+            <p class="cup">Notre bière est la toute première boisson alcoolisée totalement inoffensive pour la santé. Grâce à tous les oligo-éléments générés lors du brassage, la Greenness prévient des risques cardio-vasculaires. Au revoir la gueule de bois et bonjour les lendemains actifs ! Aucun effet secondaire lors de surconsommation de notre produit, au contraire, un état de forme optimum le lendemain se fait ressentir, non vous ne rêvez pas !! </p>
           </div>
   
           <div class="box">
             <span class="bullet-title">Protège ton portefeuille</span>
             <img class="bullet-image" src="/images/3.png" alt="Protect your wallet">
-            <p>Le rapport qualité prix de la Greennes défi toute concurrence, nous avons développé un procédé original qui permet de contenir l’équivalent de trois bières dans une. Il suffit tout simplement de mélanger chaque tiers de celle-ci avec de l’eau, goût, texture et onctuosité sont préservés. Satisfait ou remboursé, à vous de voir ! </p>
+            <p>Le rapport qualité prix de la Greenness défie toute concurrence. Nous avons développé un procédé original qui permet de contenir l’équivalent de trois bières dans une. Il suffit tout simplement de mélanger chaque tiers de celle-ci avec de l’eau, goût, texture et onctuosité sont préservés. Satisfait ou remboursé, à vous de voir ! </p>
           </div>
   </div>
 
